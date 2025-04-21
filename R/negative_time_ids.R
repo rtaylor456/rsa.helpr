1038c18a9a2efa72ec2e240336dec2b3d49ecb9f
rtaylor456 <ruth.eloisa.456@gmail.com>
2025-04-21 00:20:05 -0600

    move files back to R-scraps folder to be able to allow install

--- R/negative_time_ids.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-
-bad_ids <- metadata |>
-  filter(if_any(contains("Time_Passed"), ~ . < 0)) |>
-  pull(Participant_ID)
-
-length(bad_ids)
-
-# 11100  98902  107215 108352 108369 113798 115880 115969 118892 119223 121042
